258351a6fd356747e0f9f88198c3eae468e9948e
FatherOfGloom <antonuh2002@gmail.com>
2024-02-29 18:40:40 +0300

    Moving to dotnet 8.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Autoclicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _output = null!;
+        private bool _isAutoClickOn = false;
+        private DispatcherTimer _timer = new();
+        private Stopwatch _stopwatch = new();
+        private string _time = "00:00:00";
+        private readonly UserAPI _userAPI = new();
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            foreach (var i in Enumerable.Range(0, 1))
+            {
+                Loaded += (sender, e) => Output = "XD.";
+            }
+            _timer.Tick += OnTimerTick;
+            _timer.Interval = TimeSpan.FromMilliseconds(Int32.Parse(_intervalValueText.Text));
+            Deactivated += MainWindow_Deactivated;
+        }
+        private void MainWindow_Deactivated(object? sender, EventArgs e)
+        {
+            var window = sender as Window;
+            if (window is not null)
+            {
+                window.Topmost = true;
+            }
+        }
+        public string Output
+        {
+            get { return _output; }
+            set
+            {
+                _output += value + Environment.NewLine;
+                OnPropertyChanged();
+                _scrollViewer.ScrollToEnd();
+            }
+        }
+        public string Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool ToggleAutoClick()
+        {
+            _isAutoClickOn = !_isAutoClickOn;
+            Output = "Autoclick mode is " + (_isAutoClickOn ? "on." : "off.");
+            return _isAutoClickOn;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Output = "Program started successfully.";
+            Output = "Autoclick mode is currently " + (_isAutoClickOn ? "on." : "off.");
+        }
+        private void TextBoxInterval_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ToggleAutoClick())
+            {
+                var interval = Int32.Parse(_intervalValueText.Text);
+                Output = $"Interval is set to {interval}.";
+                _timer.Interval = TimeSpan.FromMilliseconds(interval);
+                _stopwatch.Reset();
+                _stopwatch.Start();
+                _timer.Start();
+            }
+            else
+            {
+                var timeSpan = _stopwatch.Elapsed;
+                Output = $"Autoclick was on for {String.Format("{0} h {1} min {2} sec.", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds)}";
+                _stopwatch.Stop();
+                _timer.Stop();
+            }
+        }
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            var timeSpan = _stopwatch.Elapsed;
+            Time = String.Format("{0:00}:{1:00}:{2:00}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+            _userAPI.ClickLeftMouseButton(_userAPI.MousePosition);
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName: propertyName));
+            }
+        }
+    }
+}
---
+++ UserAPI.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents.DocumentStructures;
+
+
+namespace Autoclicker
+{
+    internal class UserAPI
+    {
+        public UserAPI()
+        {
+        }
+        public Point MousePosition
+        {
+            get
+            {
+                var w32MousePosition = new Win32Point();
+                GetCursorPos(ref w32MousePosition);
+
+                return new Point(w32MousePosition.X, w32MousePosition.Y);
+            }
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(ref Win32Point pt);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwdata, int dwextrainfo);
+        [DllImport("user32.dll")]
+        static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        private enum MouseEventFlags
+        {
+            LeftDown = 2,
+            LeftUp = 4,
+        }
+        private enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+        public event EventHandler? KeyPressed;
+        public void BeginRegisterHotkeyPressed(char key, CancellationToken token)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (IsKeyPressed(key))
+                    {
+                        KeyPressed?.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            });
+        }
+        public bool IsKeyPressed(char key)
+        {
+            throw new NotImplementedException();
+        }
+        public void ClickLeftMouseButton(Point point)
+        {
+            mouse_event((int)MouseEventFlags.LeftDown, (int)point.X, (int)point.Y, 0, 0);
+            mouse_event((int)MouseEventFlags.LeftUp, (int)point.X, (int)point.Y, 0, 0);
+        }
+    }
+}
+
